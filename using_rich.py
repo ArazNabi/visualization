07f0ffb1861ef40fe826fe31fdfb9321403779fe
Araz Nabi <araznabi@gmail.com>
2023-09-21 12:32:47 +0200

    added rich-code

--- using_rich.py
+++ using_rich.py
@@ -2,5 +2,14 @@ from rich.console import Console
 
 console = Console()
 
-print([1,2,3])
-console.print([1,2,3])
+# print([1,2,3])
+# console.print([1,2,3])
+
+# console.print("[blue underline]Ser ut som en länk")
+
+# console.print("[bold]Fetstil[/bold] [italic]kursiv[/italic]")
+
+# console.print("Vit text på röd bakgrund", style="white on red")
+
+console.log("När vi använder log får vi en tidpunkt")
+console.rule("[bold red] Rubriken till ny avdelning")
