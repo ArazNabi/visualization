9a5b4b0aa5d9ebd6c30e53b1a668dd82dc587399
Araz Nabi <araznabi@gmail.com>
2023-09-21 13:48:44 +0200

    started exercise

---
+++ exercise_rich.py
@@ -0,0 +1,9 @@
+from rich.console import Console
+
+console = Console()
+
+# # Colored text
+# console.print("Warning:", style="color(11)")
+# console.print("Error:", style="color(9)")
+# console.print("Success!", style="color(10)")
+
