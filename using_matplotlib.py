507d81d381c72b2b14857361a83df6c5f51bd183
Araz Nabi <araznabi@gmail.com>
2023-09-25 10:01:38 +0200

    added more scripts

--- using_matplotlib.py
+++ using_matplotlib.py
@@ -5,15 +5,25 @@ import numpy as np
 
 # plt.show() #show figure
 
-x1 = 1
-y1 = 1
-x2 = 5
-y2 = 5
-x3 = 3
-y3 = -2
+# x1 = 1
+# y1 = 1
+# x2 = 5
+# y2 = 5
+# x3 = 3
+# y3 = -2
 
-x_values = np.array([x1,x2,x3])
-y_values = np.array([y1,y2,y3])
+# x_values = np.array([x1,x2,x3])
+# y_values = np.array([y1,y2,y3])
 
-plt.plot(x_values, y_values)
+# plt.plot(x_values, y_values, marker='v') # 'o' = circles, 's' = squares, '.' = small dots
+# plt.show()
+
+x = np.linspace(0,10,100) # creates 100 values evenlry spread from 0 to 10
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+plt.figure(figsize=(8,6)) #define width and height in inches
+ 
+plt.plot(x,y1, label='sine', color='Crimson')
+plt.plot(x,y2, label='cos', color='Blue' )
 plt.show()
