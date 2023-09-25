55d2a381a41f39a2616c6d51626f2ad2cda652e0
Araz Nabi <araznabi@gmail.com>
2023-09-25 11:22:13 +0200

    matplot exercises

---
+++ matplotlib_exercise.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# # draw a smiley face
+# def x2(x):
+#     return x**2
+
+
+# x = np.linspace(-10, 10, 100)
+# y = x2(x)
+
+# plt.plot(x,y)
+# plt.show()
+
+
+# # question 2
+# x = np.linspace(-10, 10, 100)
+
+# y = np.sin(x)
+
+# plt.plot(x,y)
+# plt.show()
+
+
+# # draw a star
+
+# x_values = np.array([1,7,2,4,6,1])
+# y_values = np.array([4,4,1,6,1,4])
+
+# plt.plot(x_values, y_values, color="Red", marker='o') # 'o' = circles, 's' = squares, '.' = small dots
+# plt.title("Twinkle little star", color="Red")
+# plt.show()
--- using_matplotlib.py
+++ using_matplotlib.py
@@ -18,12 +18,21 @@ import numpy as np
 # plt.plot(x_values, y_values, marker='v') # 'o' = circles, 's' = squares, '.' = small dots
 # plt.show()
 
-x = np.linspace(0,10,100) # creates 100 values evenlry spread from 0 to 10
+x = np.linspace(0,10,100) # creates 100 values evenly spread from 0 to 10
 y1 = np.sin(x)
 y2 = np.cos(x)
 
-plt.figure(figsize=(8,6)) #define width and height in inches
+plt.figure(figsize=(6,4)) #define width and height in inches
  
-plt.plot(x,y1, label='sine', color='Crimson')
-plt.plot(x,y2, label='cos', color='Blue' )
+
+plt.plot(x,y1, label='sine', color='Crimson', linestyle='-', marker='v') # linestyle defines the curve-style
+plt.plot(x,y2, label='cosine', color='Green', linestyle='-.', marker='s')
+
+plt.legend(loc='upper right') # shows labels in the upper right corner
+
+plt.title('Unnecessary chart', loc='left', fontsize=24) # add title, left aligned
+plt.xlabel('This is X-axis', fontsize=17) # Add label for x
+plt.ylabel('This is Y-axis', fontsize=17) # Add label for y
+
+plt.grid(True) # Show grid (duh)
 plt.show()
